ddde8a042a11dc9c4191744ca9e903d09b9655b1
zinci <zinci@10.11.107.198>
2020-09-23 10:58:34 +0200

    création du projet

--- Puzzle_Grp4_TEPE_ZINCIR/src/puzzle/FXMLDocumentController.java
+++ Puzzle_Grp4_TEPE_ZINCIR/src/puzzle/FXMLDocumentController.java
@@ -1,3 +1,5 @@
+package puzzle;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- Puzzle_Grp4_TEPE_ZINCIR/src/puzzle/Main.java
+++ Puzzle_Grp4_TEPE_ZINCIR/src/puzzle/Main.java
@@ -1,3 +1,5 @@
+package puzzle;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,7 +17,7 @@ import javafx.stage.Stage;
  *
  * @author zinci
  */
-public class main extends Application {
+public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
