e690de44922b1241c46500511e27acfb6d7ec42c
ilayd <ilayd@DESKTOP-1TU4F3D>
2020-11-25 11:04:21 +0100

    la classe Case

--- Puzzle_Grp4_TEPE_ZINCIR/src/puzzle/Case.java
+++ Puzzle_Grp4_TEPE_ZINCIR/src/puzzle/Case.java
@@ -9,7 +9,35 @@ package puzzle;
  *
  * @author ilayd
  */
-public class Case {
+public class Case extends Parametres{
     
+    private int x, y, valeur;
+    private Grille grille;
+    
+        public Case(int abs, int ord, int v) {
+        this.x = abs;
+        this.y = ord;
+        this.valeur = v;
+    }
+
+    public void setGrille(Grille g) {
+        this.grille = g;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+    
+    public void setValeur(int valeur) {
+        this.valeur = valeur;
+    }
+
+    public int getValeur() {
+        return this.valeur;
+    }
     
 }
