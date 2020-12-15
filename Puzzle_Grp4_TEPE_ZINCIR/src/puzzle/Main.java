f21de095b8e4d3915416cfd6d204c193038135dc
ilayd <ilayd@DESKTOP-1TU4F3D>
2020-12-16 00:53:14 +0100

    la classe Parametres

--- Puzzle_Grp4_TEPE_ZINCIR/src/puzzle/Main.java
+++ Puzzle_Grp4_TEPE_ZINCIR/src/puzzle/Main.java
@@ -21,22 +21,28 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        FXMLDocumentController controller = new FXMLDocumentController();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/puzzle/FXMLDocument.fxml"));
+        loader.setController(controller);
+        Parent root = loader.load();
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setTitle("Jeu du Taquin");
         stage.show();
     }
 
     /**
      * @param args the command line arguments
      */
+     
     public static void main(String[] args) {
         launch(args);
-           System.out.println("github fonctionne ?");
            
-    }
+           
+    }  
     
  
 }
